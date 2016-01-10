5b74202b5a2c559f6c49c48b811956d27ec33c26
andrewray <evilkidder@gmail.com>
2016-01-10 01:11:47 +0000

    A few more features for the lvt based n-port mems
    * Support synchronous and asynchronous read ports
      in either read-before-write or write-before-read
      mode (4 modes in total)
    
    * Each read port may have a different read mode
      independantly of the others
    
    * Seperate read/write clocks
    
    Note that in general read-before-write and
    write-before-read only really seems to make sense
    when the read and write ports are in the same clock
    domain.  Not sure what happens with real hardware
    implementations of these sorts of things but I cant
    get my head round any sort of behaviour you could
    rely on in this case.
    
    As such the various read/write/async/sync behaviours
    are only specified when the read and write clocks
    are in the same domain.  Some restrictions are
    lifted when there is only one write port and the
    memory is synchronous (this sort of mapping is most
    useful as it matches the general behaviour of fpga
    memories).
    
    Finally, although a write clock is specified per
    write port, only the 1st one is actually used (the
    LVT ram which coordinates accesses is a bank of
    registers and only 1 clock makes sense).
    
    Finally, integration.  We are still missing the
    per-bit write enable feature - we can build one
    on top (though we will have to replicate the LVT
    unnecessarily so we should really do it internally).
    Also, this should really replace the buggy version
    in HardCaml itself at some point.  Also, when some
    of the assumptions list above are not valid, we
    should assert or warn.

--- test/mram.ml
+++ test/mram.ml
@@ -1,6 +1,61 @@
-(* put together a test for the multiram component which we want to use in the 
- * memory generator logic (with some possible extensions) *)
+(* 
+ 
+  Multiport memories for HardCaml
+  ===============================
 
+  The basic memory primitive provide by HardCaml has one write (wr) and
+  one read (rd) port.  The write is synchronous and the read is asynchronous.
+  By adding a register to either the read address or output data we can
+  generate standard read-before-write and write-before-read synchronous
+  memories.  The read and write clocks can be from different domains.
+
+  A simple way to build a multi-port memory is out of simple registers
+  This is, of course, very inefficient.  One restriction that
+  still exists is that each write port must be from the same clock 
+  domain.  I (think) the reads ports could potentially be in differing
+  clock domains.
+
+  LVT
+  ===
+
+  To do a bit better we can instead build multi-port memories using
+  a Live Value Table (LVT).  The idea here is in 3 steps;
+
+  1] To make N rd ports, we replicate a 1 rd ram N times each written
+     with the same data.  Note each replicated RAM will contain exactly
+     the same data.  The replication provides N access ports to this data.
+     Call this memory_nrd.
+
+  2] To make M wr ports we initially replicate memory_nrd M times.
+     Each write port is connected to one memory_nrd instance.  Note that
+     each memory_nrd bank will this time contain different data.
+     We now have M * N ram outputs to select between to for the N read
+     ports.
+
+  3] Build the live value table.  This tracks the bank which was most 
+     recently written for each address in the RAM.  This is built as
+     a multi-port memory itself, specifically using the simple register
+     scheme described before.  The outputs of the LVT (one for each read
+     port) selects the bank built in step 2 that contains the most 
+     recently written value for a particular address.
+
+  Consider 2 read, 4 write ports on a 256 element x 32 bit memory.  
+  Using the pure register scheme we will need to generate 256x32 
+  register bits, write port selection logic at the input to each 
+  register, and 2 256x32 muxes to select the read data.
+
+  With the LVT scheme we still need a register based multiport
+  memory - in this case 2 read, 4 write ports on a 256 element x 2 bit
+  memory.  Note the change here - we went from a 32 bit memory (to
+  store the data) to a 2 bit memory (to store the index of the latest
+  write bank ie it's of width log2(number of write ports).  In this
+  case we have save approx 16x logic resources.
+
+  Of course we also need 2*4 256 x 32 memories (with 1 read and 1 write
+  port each) to store the data.
+
+
+*)
 open HardCaml
 open Signal
 open Comb
@@ -9,35 +64,42 @@ module type Cfg = sig
   val abits : int
   val dbits : int
   val size : int
-  val spec : Types.register
 end
 
 module Wr = interface
-  we wa d
+  wclk we wa d
 end
 
 module Rd = interface
-  re ra
+  rclk re ra
 end
+
+(* fallthrough = wbr (write before read) *)
+type mode = [ `async_rbw | `async_wbr | `sync_rbw | `sync_wbr ]
+
+let is_sync = function `sync_wbr | `sync_rbw -> true | _ -> false
+let is_async m = not (is_sync m)
+let is_rbw = function `sync_rbw | `async_rbw -> true | _ -> false
+let is_wbr m = not (is_rbw m)
 
 module Ports(C : Cfg) = struct
   module Wr = struct
     include Wr 
-    let bits = { we = 1; wa = C.abits; d = C.dbits }
+    let bits = { wclk = 1; we = 1; wa = C.abits; d = C.dbits }
     let t = map2 (fun (n,_) b -> n,b) t bits
   end
   module Rd = struct
     include Rd 
-    let bits = { re = 1; ra = C.abits }
+    let bits = { rclk = 1; re = 1; ra = C.abits }
     let t = map2 (fun (n,_) b -> n,b) t bits
   end
 end
 
-module Memory_regs(C : Cfg) : sig
+module Multiport_regs(C : Cfg) : sig
   
   include module type of Ports(C)
 
-  val memory_nwr_nrd : wr:t Wr.t array -> rd:t Rd.t array -> fallthrough:bool array -> t array
+  val memory : wr:t Wr.t array -> rd:t Rd.t array -> mode:mode array -> t array
 
 end = struct
 
@@ -54,29 +116,35 @@ end = struct
                            | [s0,d0;s1,d1] -> s1 |: s0, mux2 s1 d1 d0
                            | _ -> empty,empty)
 
+  let spec clk = { Seq.r_none with Types.reg_clock = clk }
+
   let reg_we_enable ~we ~wa = 
     select (binary_to_onehot wa) (size-1) 0 &: mux2 we (ones size) (zero size)
 
   let memory_nwr_array ~wr = 
+    let wclk = wr.(0).wclk in (* note; we can only support one write clock! *)
     let wr = Array.to_list wr in
     let we1h = List.map (fun wr -> reg_we_enable ~we:wr.we ~wa:wr.wa) wr in
     Array.to_list @@ Array.init size 
       (fun elt ->
         let wed = List.map2 (fun we1h wr -> we1h.[elt:elt], wr.d) we1h wr in
         let we,d = pri wed in (* last d with write enable set *)
-        let r = Seq.reg spec we d in
+        let r = Seq.reg (spec wclk) we d in
         we, d, r)
 
   (* n write, n read ports *)
-  let memory_nwr_nrd ~wr ~rd ~fallthrough = 
+  let memory ~wr ~rd ~mode = 
     let base = memory_nwr_array ~wr in
     Array.init (Array.length rd) 
       (fun i -> 
-        let base = List.map 
-          (fun (we,d,r) -> if fallthrough.(i) then mux2 we d r else r) 
-          base
-        in
-        mux rd.(i).ra base) 
+        let reg = Seq.reg (spec rd.(i).rclk) in
+        let mr = List.map (fun (we,d,r) -> mux2 we d r) base in
+        let r = List.map (fun (_,_,r) -> r) base in
+        match mode.(i) with
+        | `async_wbr -> mux rd.(i).ra mr
+        | `async_rbw -> mux rd.(i).ra r
+        | `sync_wbr -> mux (reg rd.(i).re rd.(i).ra) r
+        | `sync_rbw -> reg rd.(i).re (mux rd.(i).ra r))
 
 end
 
@@ -86,44 +154,41 @@ module Lvt(C : Cfg) = struct
   open Wr
   open Rd
 
-  let memory_nrd ~wr ~rd ~fallthrough = 
-    let nrd = Array.length rd in
-    Array.init nrd (fun i ->
-      let ram = if fallthrough.(i) then Seq.ram_wbr else Seq.ram_rbw in 
-      let rd = rd.(i) in
-      ram ~spec:C.spec ~size:C.size
-        ~we:wr.we ~wa:wr.wa ~d:wr.d 
-        ~re:rd.re ~ra:rd.ra) 
+  let spec clk = { Seq.r_none with Types.reg_clock = clk }
 
-  let memory_nwr_nrd ~wr ~rd ~fallthrough = 
+  let memory_1rd ~wr ~rd ~mode = 
+    let wspec, rspec = spec wr.wclk, spec rd.rclk in
+    match mode with
+    | `sync_rbw -> Seq.reg rspec rd.re (Seq.memory C.size wspec wr.we wr.wa wr.d rd.ra)
+    | `sync_wbr -> Seq.memory C.size wspec wr.we wr.wa wr.d (Seq.reg rspec rd.re rd.ra)
+    | `async_rbw -> Seq.memory C.size wspec wr.we wr.wa wr.d rd.ra
+    | `async_wbr -> 
+      mux2 (wr.we &: (wr.wa ==: rd.ra)) wr.d (Seq.memory C.size wspec wr.we wr.wa wr.d rd.ra)
+
+  let memory_nrd ~wr ~rd ~mode = 
+    let nrd = Array.length rd in
+    Array.init nrd (fun i -> memory_1rd ~wr ~rd:rd.(i) ~mode:mode.(i))
+
+  let memory_nwr_nrd ~wr ~rd ~mode = 
     let nwr, nrd = Array.length wr, Array.length rd in
-    assert (Array.length fallthrough = nrd);
+    assert (Array.length mode = nrd);
 
     (* create the live value table *)
     let module Lvt_cfg = struct
       let abits = C.abits
       let dbits = Utils.clog2 nwr
       let size = C.size
-      let spec = C.spec
     end in
-    let module Lvt = Memory_regs(Lvt_cfg) in
+    let module Lvt = Multiport_regs(Lvt_cfg) in
     let lvt = 
       if nwr = 1 then [||]
       else
         let lvt_wr = Array.init nwr (fun i -> { wr.(i) with d = consti Lvt_cfg.dbits i; }) in
-        let lvt_rd = Array.init nrd (fun i -> 
-          { rd.(i) with ra = 
-              if fallthrough.(i) then Seq.reg C.spec rd.(i).re rd.(i).ra else rd.(i).ra 
-          })
-        in
-        let lvt = Lvt.memory_nwr_nrd ~wr:lvt_wr ~rd:lvt_rd ~fallthrough:(Array.make nrd false) in
-        Array.init nrd (fun i -> 
-          if fallthrough.(i) then lvt.(i)
-          else Seq.reg C.spec rd.(i).re lvt.(i)) 
+        Lvt.memory ~wr:lvt_wr ~rd ~mode 
     in
 
     (* create the memory banks *)
-    let mem = Array.init nwr (fun i -> memory_nrd ~wr:wr.(i) ~rd ~fallthrough) in
+    let mem = Array.init nwr (fun i -> memory_nrd ~wr:wr.(i) ~rd ~mode) in
 
     (* select the correct memory bank *)
     Array.init nrd 
@@ -149,10 +214,9 @@ let abits = ref 4
 let dbits = ref 8
 let nwr = ref 2
 let nrd = ref 2
-let lvt = ref true
 let random_re = ref true
-let all_fall = ref false
-let none_fall = ref false
+let mode : [ `alternate | mode ] ref = ref `alternate
+let vlog = ref false
 
 let _ = Arg.(parse [
   "-n", Set_int(n_cycles), "Number of cycles to simulate (default: 1000)";
@@ -160,10 +224,12 @@ let _ = Arg.(parse [
   "-d", Set_int(dbits), "data bits (default: 8)";
   "-rd", Set_int(nrd), "read ports (default: 2)";
   "-wr", Set_int(nwr), "write ports (default: 2)";
-  (*"-regs", Clear(lvt), "build async register based memory (default: lvt)";*)
   "-no-re", Clear(random_re), "disable random read-enable toggling (default: false)";
-  "-fallthrough", Set(all_fall), "Put all ports in fallthrough mode (default: alternate)";
-  "-no-fallthrough", Set(none_fall), "Put all ports in non-fallthrough mode (default: alternate)";
+  "-sync-rbw", Unit(fun () -> mode := `sync_rbw), "Put all ports in sync_rbw mode";
+  "-sync-wbr", Unit(fun () -> mode := `sync_wbr), "Put all ports in sync_wbr mode";
+  "-async-rbw", Unit(fun () -> mode := `async_rbw), "Put all ports in async_rbw mode";
+  "-async-wbr", Unit(fun () -> mode := `async_wbr), "Put all ports in async_wbr mode";
+  "-vlog", Set(vlog), "Dump verilog";
 ] (fun _ -> failwith "invalid anon arg")
 "LVT Multiport Memory Testbench.
 
@@ -172,9 +238,9 @@ port memories (as available in FPGAs).  Requires N*M base memories
 plus a so called Live Value Table to directs reads to the most
 recently accessed write data.
 
-Each port may be set independantly in read-before-write or 
-write-before-read mode (by default the testbench alternates port 
-modes).
+Each port may be set independently in syncronous or asynchronous read mode with
+read-before-write or write-before-read behaviour (by default the testbench alternates 
+between port modes).
 
 For testing the read-enable port may be held constant.
 ")
@@ -185,27 +251,35 @@ let testbench_mem_regs () =
     let abits = !abits
     let dbits = !dbits
     let size = (1 lsl abits)
-    let spec = Seq.r_none
   end in
   let nwr = !nwr in
   let nrd = !nrd in
-  let fallthrough = Array.init nrd (fun i -> 
-    if !all_fall then true
-    else if !none_fall then false
-    else i mod 2 = 1) in
-  let lvt = !lvt in
+  let mode = Array.init nrd (fun i ->
+    match !mode with
+    | `alternate -> 
+        (match i mod 4 with 0 -> `sync_rbw | 1 -> `sync_wbr | 2 -> `async_rbw | _ -> `async_wbr)
+    | `sync_rbw -> `sync_rbw
+    | `sync_wbr -> `sync_wbr
+    | `async_rbw -> `async_rbw
+    | `async_wbr -> `async_wbr) 
+  in
   let random_re = !random_re in
 
-  let module M = Memory_regs(C) in
+  (*let module M = Memory_regs(C) in*)
   let module L = Lvt(C) in
   let aname m n = n ^ string_of_int m in
   let mk_input m (n,b) = input (aname m n) b in
-  let wr = Array.init nwr (fun m -> M.Wr.(map (mk_input m) t)) in
-  let rd = Array.init nrd (fun m -> M.Rd.(map (mk_input m) t)) in
-  let q = (if lvt then L.memory_nwr_nrd else M.memory_nwr_nrd) ~wr ~rd ~fallthrough in
+  let wr = Array.init nwr (fun m -> L.Wr.(map (mk_input m) t)) in
+  let rd = Array.init nrd (fun m -> L.Rd.(map (mk_input m) t)) in
+  let q = L.memory_nwr_nrd ~wr ~rd ~mode in
   let qi = Array.init nrd (fun m -> input (aname m "qi") C.dbits) in
-  let reg_qr m = Seq.reg C.spec (if fallthrough.(m) then vdd else rd.(m).M.Rd.re) qi.(m) in
-  let qr = Array.init nrd (if lvt then reg_qr else (fun m -> qi.(m))) in
+  let reg_qr m = 
+    match mode.(m) with
+    | `sync_wbr -> Seq.reg Seq.r_none vdd qi.(m)
+    | `sync_rbw -> Seq.reg Seq.r_none rd.(m).L.Rd.re qi.(m)
+    | _ -> qi.(m)
+  in
+  let qr = Array.init nrd reg_qr in
   let qs = Array.init nrd (fun m -> 
     output (aname m "q") q.(m), 
     output (aname m "qr") qr.(m))
@@ -223,14 +297,16 @@ let testbench_mem_regs () =
  
   let rd m = 
     {
-      M.Rd.ra = S.in_port sim (aname m "ra");
+      L.Rd.rclk = ref B.gnd;
+      ra = S.in_port sim (aname m "ra");
       re = try S.in_port sim (aname m "re") with _ -> ref B.vdd;
     }
   in
 
   let wr m = 
     {
-      M.Wr.wa = S.in_port sim (aname m "wa");
+      L.Wr.wclk = ref B.gnd;
+      wa = S.in_port sim (aname m "wa");
       we = S.in_port sim (aname m "we");
       d = S.in_port sim (aname m "d");
     }
@@ -242,11 +318,11 @@ let testbench_mem_regs () =
 
   (* random reads and writes *)
   let rand (_,b) = b, Random.int (1 lsl b) in
-  let rand_wr () = M.Wr.(map rand t) in
+  let rand_wr () = L.Wr.(map rand t) in
   let rand_rd () = 
-    if random_re then M.Rd.(map rand t) 
+    if random_re then L.Rd.(map rand t) 
     else
-      M.Rd.{ map rand t with re = 1, 1 } 
+      L.Rd.{ map rand t with re = 1, 1 } 
   in
 
   (* implement the reads and writes *)
@@ -256,28 +332,31 @@ let testbench_mem_regs () =
   let assign a (b,c) = a := B.consti b c in
 
   let update_write wr = 
-    let wr = M.Wr.(map snd wr) in
-    if wr.M.Wr.we = 1 then 
-      ref_mem.(wr.M.Wr.wa) <- wr.M.Wr.d
+    let wr = L.Wr.(map snd wr) in
+    if wr.L.Wr.we = 1 then 
+      ref_mem.(wr.L.Wr.wa) <- wr.L.Wr.d
   in
   let prev_ra = Array.make nrd 0 in
   let update_read i rd = 
-    (*let rd = M.Rd.({ map snd rd with M.Rd.re = 1 }) in*)
-    let rd = M.Rd.(map snd rd) in
-    if rd.M.Rd.re = 1 then begin
-      qi.(i) := B.consti C.dbits ref_mem.(rd.M.Rd.ra);
-      prev_ra.(i) <- rd.M.Rd.ra
-    end else if fallthrough.(i) then begin
+    (*let rd = L.Rd.({ map snd rd with L.Rd.re = 1 }) in*)
+    let rd = L.Rd.(map snd rd) in
+    if rd.L.Rd.re = 1 || is_async mode.(i) then begin
+      qi.(i) := B.consti C.dbits ref_mem.(rd.L.Rd.ra);
+      prev_ra.(i) <- rd.L.Rd.ra
+    end else if mode.(i) = `sync_wbr then begin
       qi.(i) := B.consti C.dbits ref_mem.(prev_ra.(i))
     end
   in
 
   let perform_reads ft = 
+    let cond = function `sync_wbr | `async_wbr -> ft
+                      | _ -> not ft
+    in
     for i=0 to nrd-1 do
-      if fallthrough.(i) = ft then begin
+      if cond mode.(i) then begin
         let rd' = rand_rd() in
         update_read i rd';
-        ignore @@ M.Rd.(map2 assign rd.(i) rd')
+        ignore @@ L.Rd.(map2 assign rd.(i) rd')
       end
     done;
   in
@@ -286,7 +365,7 @@ let testbench_mem_regs () =
     for i=0 to nwr-1 do
       let wr' = rand_wr() in
       update_write wr';
-      ignore @@ M.Wr.(map2 assign wr.(i) wr')
+      ignore @@ L.Wr.(map2 assign wr.(i) wr')
     done;
   in
 
@@ -297,7 +376,8 @@ let testbench_mem_regs () =
     S.cycle sim;
   done;
 
-  Lwt_main.run (Waveterm_ui.run Waveterm_waves.({ cfg=default; waves })) 
+  Lwt_main.run (Waveterm_ui.run Waveterm_waves.({ cfg=default; waves }));
+  (if !vlog then Rtl.Verilog.write print_string circ)
 
 
 let () = testbench_mem_regs ()
