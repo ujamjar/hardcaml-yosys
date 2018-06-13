4c39b63aafa3c041f664ff2b3d408eaa59971710
Andrew Ray <evilkidder@gmail.com>
2018-06-13 10:24:00 +0100

    update to latest libraries

--- src/io.ml
+++ src/io.ml
@@ -1,10 +1,9 @@
 (* read/write json files *)
 
-let read chan = 
+let read chan =
   let st = Yojson.init_lexer () in
   let buf = Lexing.from_channel chan in
   Yosys_atd_j.read_t st buf
 
-let write chan t = 
-  Ag_util.Json.to_channel Yosys_atd_j.write_t chan t
-
+let write chan t =
+  Atdgen_runtime.Util.Json.to_channel Yosys_atd_j.write_t chan t
